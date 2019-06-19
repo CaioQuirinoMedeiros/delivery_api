f35a3b84e73a44053306a052fa85d8a0f328e9eb
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-19 17:48:40 -0300

    Removing role field

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -6,7 +6,7 @@ class UserController {
   async store ({ request, auth }) {
     const data = await request.only(['name', 'email', 'password'])
 
-    await User.create({ ...data, role: 'client' })
+    await User.create(data)
 
     const token = await auth.attempt(data.email, data.password)
 
