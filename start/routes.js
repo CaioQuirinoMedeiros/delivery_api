0a071ec57abc5931d073d504be851305f6fbe9e3
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-19 15:37:52 -0300

    Adding user validator and sizes routes

--- start/routes.js
+++ start/routes.js
@@ -3,13 +3,15 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('users', 'UserController.store')
+Route.post('users', 'UserController.store').validator('User')
 Route.post('sessions', 'SessionController.store').validator('Session')
 
 Route.group(() => {
   Route.resource('categories', 'CategoryController').apiOnly()
 
   Route.resource('products', 'ProductController').apiOnly()
+
+  Route.resource('sizes', 'SizeController').apiOnly()
 
   Route.resource('images', 'ImageController').apiOnly()
 }).middleware('auth')
